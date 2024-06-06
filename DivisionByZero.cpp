957b9890fda8b7a8a9417a916a131c3af1bcb77f
Sapphire Haeward <76604887+sapphire-haeward@users.noreply.github.com>
2024-06-06 09:58:00 -0400

    Add files via upload

---
+++ DivisionByZero.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//if else statements
+using namespace std;
+
+int main ()
+{
+
+int score;
+cout << "Enter your score!\n";
+cin >> score;
+
+if (score > 100)
+cout << "High\n";
+if (score <= 100) 
+cout << "Low\n" ;
+
+return 5;
+}
